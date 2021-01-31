f952cde02a3db2435dcf98b07f9dd23627213295
Babaev <babaevstudied@gmail.com>
2021-02-01 00:07:39 +0300

    code style

--- longStr.cpp
+++ longStr.cpp
@@ -7,11 +7,7 @@
 
 char * ltoab(long num,char * c){
     for (int i = 0; i < (sizeof(num)*BIT); ++i){
-        if ((num&1<<(sizeof(num)*BIT - i - 1)) != 0){
-            c[i] = '1';
-        }else{
-            c[i] = '0';
-        }
+       c[i] = static_cast<bool>(num & 1 << (sizeof(num) * BIT - i - 1)) ? '1' : '0';
 
     }
     return c;
